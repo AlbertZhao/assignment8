646c02eb5a6a7cd384a20f28bcdd431e7b7e2d81
albert.zhao <albert.chiu.cn@gmail.com>
2019-09-08 17:53:56 +0800

    init

---
+++ assignment8/src/main/java/com/zhaoshijie/App.java
@@ -0,0 +1,13 @@
+package com.zhaoshijie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
